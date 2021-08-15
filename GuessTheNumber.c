d7c5570c7b6d0c82a13b8b3a945db3ea2f08a3e1
Elise <joy353087539@gmail.com>
2021-08-15 15:34:23 -0400

    Improved_Version

--- GuessTheNumber.c
+++ GuessTheNumber.c
@@ -6,19 +6,19 @@ int main()
 {
     int randomNumber = 0;
     int guess = 0;
-    int numberOfGuesses = 5;
+    int numberOfGuesses = 10;
     time_t t;
 
     // Initialization of random number generator
     srand((unsigned)time(&t));
 
     // Get the random number
-    randomNumber = rand() % 21;
+    randomNumber = rand() % 100;
 
     printf("\nThis is a guessing game.");
-    printf("\nI have chosen a number between 0 and 20, which you must guess. \n");
+    printf("\nI have chosen a number between 0 and 100, which you must guess. \n");
 
-    for (numberOfGuesses = 5; numberOfGuesses > 0; --numberOfGuesses)
+    for (numberOfGuesses = 10; numberOfGuesses > 0; --numberOfGuesses)
     {
         printf("\nYou have %d tr%s left.", numberOfGuesses, numberOfGuesses == 1 ? "y" : "ies");
         printf("\nEnter a guess: ");
@@ -27,16 +27,16 @@ int main()
         if (guess == randomNumber)
         {
             printf("\nCongratulations, you guessed it!\n");
-            return;
+            return 0;
         }
-        else if (guess < 0 || guess > 20) // checking for an invalid guess
-            printf("I said the number is between 0 and 20.\n");
+        else if (guess < 0 || guess > 100) // checking for an invalid guess
+            printf("I said the number is between 0 and 100.\n");
         else if (randomNumber > guess)
             printf("Sorry, %d is wrong. My number is greater than that.\n", guess);
         else if (randomNumber < guess)
             printf("Sorry, %d is wrong. My number is less than that. \n", guess);
     }
-    printf("\nYou have had five tries and failed. The number was %d\n", randomNumber);
+    printf("\nYou have had 10 tries and failedm, you're stupid. The number was %d\n", randomNumber);
 
     printf("\n");
 
